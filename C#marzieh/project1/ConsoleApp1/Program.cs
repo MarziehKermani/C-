fca3bfed90407ed56eaa63e17eea0ff4eb11d002
MarziehKermani <Marzieh.kermani13@gmail.com>
2025-10-14 15:16:17 +0330

    Add files via upload

---
+++ C#marzieh/project1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //مرضیه کرمانی //
+            Console.WriteLine("Enter a Celsius :");  
+
+            string input = Console.ReadLine();  
+            double c = Convert.ToDouble(input); 
+
+            double f = (c * 9 / 5) + 32;  
+
+            Console.WriteLine("Fahrenheit: " + f);  
+
+            Console.ReadLine(); 
+        }
+    }
+}
---
+++ C#marzieh/project2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class MyClass  //مرضیه کرمانی
+    {
+        static void Main()  
+        {
+            Console.WriteLine("hi enter a number:"); 
+
+            string input = Console.ReadLine();  
+            double r = Convert.ToDouble(input); 
+
+            double masahat = 3.14 * r * r;  
+            double mohit = 2 * 3.14 * r;   
+
+            Console.WriteLine("masahat: " + masahat); 
+            Console.WriteLine("mohit: " + mohit);
+
+            Console.ReadLine();  
+        }
+    }
+}
