e992ed9b986d781ea10f6d0aa4d79d1917c9fbcd
MarziehKermani <Marzieh.kermani13@gmail.com>
2025-10-22 09:38:30 +0330

    Add files via upload

---
+++ C#marzieh/5/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+            //مرضیه کرمانی
+        {
+            Console.Write("Enter a number : ");
+            int number = int.Parse(Console.ReadLine());
+
+            switch (number)
+            {
+                case 1:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 2:
+                    Console.WriteLine("Sunday");
+                    break;
+                case 3:
+                    Console.WriteLine("Monday");
+                    break;
+                case 4:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 5:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 6:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 7:
+                    Console.WriteLine("Friday");
+                    break;
+                default:
+                    Console.WriteLine("END!");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
