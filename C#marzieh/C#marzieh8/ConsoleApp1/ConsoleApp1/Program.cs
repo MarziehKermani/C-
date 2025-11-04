784ca589fbf149d815451da7875d3b0a01ab5820
MarziehKermani <Marzieh.kermani13@gmail.com>
2025-11-04 22:59:47 +0330

    Add files via upload

---
+++ C#marzieh/C#marzieh8/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.Write($"number  {i} enter a number : ");
+                number = int.Parse(Console.ReadLine());
+
+
+                if (number % 2 == 0)
+                    Console.WriteLine(" number even.");
+                else
+                    Console.WriteLine("numbrt odd");
+
+                Console.WriteLine();
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
