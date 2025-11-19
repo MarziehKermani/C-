242de7d7a295733bc0adcafb22e0d9a5f1c14de4
MarziehKermani <marziehkermani13@gmail.com>
2025-11-19 22:06:42 +0330

    fix bug

---
+++ C#marzieh/ConsoleApp12/ConsoleApp12/ConsoleApp12/ProgramBase.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+[DebuggerDisplay("{GetDebuggerDisplay(),nq}")]
+internal class ProgramBase
+{
+    private static void Main(string[] args)
+    {
+        TEST();
+
+        static void TEST()
+        {
+            // See https://aka.ms/new-console-template for more information
+            Console.WriteLine("Hello, World!");
+            void f()
+            {
+                Console.WriteLine("HAIIIIIIIIIIIIIIIIIIIIII");
+            }
+        }
+    }
+
+    private string GetDebuggerDisplay()
+    {
+        return ToString();
+    }
+}
